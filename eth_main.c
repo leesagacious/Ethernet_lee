0509b07b3ded736b105019a099d24ccd85ea7d36
li Insects Awaken <zhex.h.li@intel.com>
2022-01-01 22:34:20 +0800

    ready to init queue

--- eth_main.c
+++ eth_main.c
@@ -1,3 +1,14 @@
+#include "eth.h"
+
+static int eth_init(struct platform_device *pdev)
+{
+	unsigned int i;
+	
+	for (i = 0; i < MAX_GENER_QUEUES; i++) {
+		
+	}
+}
+
 static int eth_probe(struct platform_device *pdev)
 {
 
