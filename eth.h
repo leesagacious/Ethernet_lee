9d94601c8e3d904ac60c35a948b359de5995c8a0
li Insects Awaken <zhex.h.li@intel.com>
2022-01-01 22:26:50 +0800

    add napi for queue

--- eth.h
+++ eth.h
@@ -1,9 +1,14 @@
+#include <linux/netdevice.h>
+
 #define MAX_GENER_QUEUES  4
 
 struct gener_queue {
   struct gener_eth *geth;
+  struct napi_struct napi;
 };
 
 struct gener_eth {
+  unsigned int mask;
+  unsigned int num_queues;
   struct gener_queue  queues[MAX_GENER_QUEUES];
 };
