deecd6a277fd9c937881a437ee771c92b4a516cd
li Insects Awaken <zhex.h.li@intel.com>
2022-01-21 00:07:49 +0800

    get mac from net_device's private data

--- eth_main.c
+++ eth_main.c
@@ -3,6 +3,12 @@
 static int eth_init(struct platform_device *pdev)
 {
 	unsigned int i;
+       /*
+        * 获取net_device 结构. 其中的一个目的是要根据它获取mac.
+	* 因为mac 存在于它空间的私有数据中了.
+	*/
+	struct net_device *ndev = platform_get_drvdata(pdev);
+	struct gener_eth *g_eth = netdev_priv(ndev);
 	
 	for (i = 0; i < MAX_GENER_QUEUES; i++) {
 		
